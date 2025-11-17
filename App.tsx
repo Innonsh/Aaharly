70c5b463d1354f30be62cb48f0fa01b201db3a9f
3MaheshKadam <maheshkadam9298@gmail.com>
2025-11-17 15:11:06 +0530

    structure of  project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,26 @@
-import { ScreenContent } from 'components/ScreenContent';
-import { StatusBar } from 'expo-status-bar';
+// import { ScreenContent } from 'components/ScreenContent';
+// import { StatusBar } from 'expo-status-bar';
 
+// import './global.css';
+
+// export default function App() {
+//   return (
+//     <>
+//       <ScreenContent title="Home" path="App.tsx"></ScreenContent>
+//       <StatusBar style="auto" />
+//     </>
+//   );
+// }
+import { View, Text } from 'react-native'
+import React from 'react'
 import './global.css';
 
-export default function App() {
+const App = () => {
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
-  );
+    <View className='flex-1 bg-slate-600'>
+      <Text>App</Text>
+    </View>
+  )
 }
+
+export default App
