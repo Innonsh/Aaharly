16d9846e3f0fefdfc905190b6b73fd986b7b7e6a
Shashank Shinde <shashank.shinde@live.com>
2025-11-20 15:18:46 +0530

    implement theming and font support in the app; add Matter font assets and configure themes for light and dark modes. Update App component structure to utilize react-native-paper for theming. Include necessary dependencies in package.json and set up asset linking for both Android and iOS.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from "react-native";
+import React, { useMemo, useState } from "react";
+import {
+  DefaultTheme,
+  MD3DarkTheme,
+  PaperProvider,
+  Text,
+} from "react-native-paper";
+import { ThemeMode } from "./src/models/interface";
+import { fontConfig } from "./src/theme/Fonts";
+import { Colors, themeColors } from "./src/theme/Colors";
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
   },
-})
+});
+
+const AppContent = () => {
+  const [themeMode, setThemeMode] = useState<ThemeMode>(ThemeMode.Light);
+
+  const theme = useMemo(() => {
+    if (themeMode === ThemeMode.Dark) {
+      return {
+        ...MD3DarkTheme,
+        dark: true,
+        fonts: fontConfig,
+        roundness: 1,
+        colors: {
+          ...MD3DarkTheme.colors,
+          primary: Colors.primary,
+          accent: Colors.secondary,
+          background: themeColors.dark.background,
+          elevation: {
+            level1: "red",
+          },
+        },
+      };
+    }
+    return {
+      ...DefaultTheme,
+      dark: false,
+      fonts: fontConfig,
+      roundness: 1,
+      colors: {
+        ...DefaultTheme.colors,
+        primary: Colors.primary,
+        accent: Colors.secondary,
+        background: themeColors.light.background,
+      },
+    };
+  }, [themeMode]);
+
+  return (
+    <PaperProvider theme={theme}>
+      <SafeAreaView>
+        <Text>App</Text>
+        </SafeAreaView>
+    </PaperProvider>
+  );
+};
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Text>App</Text>
-    </View>
-  )
-}
+  return <AppContent />;
+};
 
-export default App
+export default App;
---
+++ src/theme/Fonts.ts
@@ -0,0 +1,45 @@
+export const fonts = {
+  Regular: 'Matter-Regular',
+  Medium: 'Matter-Medium',
+  Bold: 'Matter-Bold',
+  SemiBold: 'Matter-SemiBold',
+  Light: 'Matter-Light',
+};
+
+export const fontConfig = {
+  default: {
+    fontFamily: fonts.Regular,
+    fontWeight: 'normal' as const,
+    letterSpacing: 0,
+  },
+  bodySmall: {
+    fontFamily: fonts.Regular,
+    fontWeight: 'normal' as const,
+    letterSpacing: 0,
+  },
+  labelLarge: {
+    fontFamily: fonts.Regular,
+    fontWeight: 'normal' as const,
+    letterSpacing: 0,
+  },
+  bodyLarge: {
+    fontFamily: fonts.Regular,
+    fontWeight: 'normal' as const,
+    letterSpacing: 0,
+  },
+  regular: {
+    fontFamily: fonts.Regular,
+    fontWeight: 'normal' as const,
+    letterSpacing: 0,
+  },
+  medium: {
+    fontFamily: fonts.SemiBold,
+    fontWeight: '500' as const,
+    letterSpacing: 0,
+  },
+  bold: {
+    fontFamily: fonts.Bold,
+    fontWeight: 'bold' as const,
+    letterSpacing: 0.2,
+  },
+};
