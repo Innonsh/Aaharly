10e0ab48efee3d34f162d4b5cdcb99460d16f48b
lokeeklokhandeinnonsh <lokeek.innonsh@gmail.com>
2025-12-04 12:47:17 +0530

    added route

--- src/navigation/NavigationRoutes.ts
+++ src/navigation/NavigationRoutes.ts
@@ -2,23 +2,24 @@ export enum NavigationRoutes {
   SPLASH = "SPLASH",
   // ONBOARDING = "ONBOARDING",
   LOGIN = "LOGIN",
-  OTP ="OTP",
+  OTP = "OTP",
   LoginWithEmail = "Email",
-  EMAIL_VERIFICATION ="Email_Verification",
+  EMAIL_VERIFICATION = "Email_Verification",
   PROFILE_SETUP1 = "PROFILE_SETUP1",
   PROFILE_SETUP2 = "PROFILE_SETUP2",
   PROFILE_SETUP3 = "PROFILE_STEUP3",
   HOME = "HOME",
+  ONBOARDING = "ONBOARDING",
 
   // HOME = "HOME", // optional – use whatever your main screen is
 }
 export type RootStackParamList = {
-  [NavigationRoutes.SPLASH]:undefined;
+  [NavigationRoutes.SPLASH]: undefined;
   [NavigationRoutes.LOGIN]: undefined;
   [NavigationRoutes.OTP]: { phone: string };
-  [NavigationRoutes.LoginWithEmail]:undefined;
-  [NavigationRoutes.EMAIL_VERIFICATION]: {email: string};
-  // [NavigationRoutes.ONBOARDING]: undefined;
+  [NavigationRoutes.LoginWithEmail]: undefined;
+  [NavigationRoutes.EMAIL_VERIFICATION]: { email: string };
+  [NavigationRoutes.ONBOARDING]: undefined;
 
   [NavigationRoutes.PROFILE_SETUP1]: undefined;
   [NavigationRoutes.PROFILE_SETUP2]: undefined;
